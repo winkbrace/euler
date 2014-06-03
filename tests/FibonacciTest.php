762a39e2950323bff35ccd2ef5c34cc4f926d3ba
Bas de Ruiter <winkbrace@gmail.com>
2014-06-03 16:50:32 +0200

    put vendor in gitignore

---
+++ tests/FibonacciTest.php
@@ -0,0 +1,6 @@
+<?php namespace Math; 
+
+class FibonacciTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
---
+++ tests/PrimeTest.php
@@ -0,0 +1,6 @@
+<?php namespace tests; 
+
+class PrimeTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
