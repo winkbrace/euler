b3a88453d3effe3ba0514583d673423fac2cee97
Bas de Ruiter <winkbrace@gmail.com>
2014-06-05 13:46:04 +0200

    solved 19

--- problems/01/p019.php
+++ problems/01/p019.php
@@ -18,3 +18,18 @@
  * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
  */
 
+$months = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+$count = 0;
+$totalRemainders = 1; // jan 1 1900 is monday, so remainder = 1
+
+for ($year = 1900; $year < 2001; $year++) {
+    $months[1] = ($year % 4 == 0 && $year != 1900) ? 29 : 28;
+
+    foreach ($months as $month) {
+        $totalRemainders += $month % 7;
+        if ($totalRemainders % 7 == 0 && $year != 1900)
+            $count++;
+    }
+}
+
+echo $count;
