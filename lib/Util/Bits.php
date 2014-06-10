665bb8366f28f1889e37f47c51156819a23d1d2d
Bas de Ruiter <winkbrace@gmail.com>
2014-06-10 09:25:06 +0200

    created Bits helper

--- lib/Util/Bits.php
+++ lib/Util/Bits.php
@@ -2,5 +2,23 @@
 
 class Bits
 {
-    
+    /**
+     * find the binary ones in a given number
+     * @param int $n
+     * @return array
+     */
+    public static function getOnPositions($n)
+    {
+        $positions = array();
+        // move from highest power of 2 to 0 to get the positions ordered from left to right
+        $max = (int) floor(log($n, 2));
+        for ($i=$max; $i>=0; $i--)
+        {
+            $p = pow(2, $i);
+            if ($n & $p)
+                $positions[] = $max - $i;
+        }
+
+        return $positions;
+    }
 } 
--- tests/Util/BitsTest.php
+++ tests/Util/BitsTest.php
@@ -1,6 +1,21 @@
 <?php namespace Util; 
 
-class BitsTest extends \PHPUnit_Framework_TestCase {
+class BitsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetOnPositions()
+    {
+        // 19 = 10011
+        $positions = Bits::getOnPositions(19);
+        $expected = array(0, 3, 4);
+        $this->assertEquals($expected, $positions);
 
+        // 2 = 10
+        $positions = Bits::getOnPositions(2);
+        $expected = array(0);
+        $this->assertEquals($expected, $positions);
+
+        $positions = Bits::getOnPositions(0b111001);
+        $expected = array(0, 1, 2, 5);
+        $this->assertEquals($expected, $positions);
+    }
 }
- 
