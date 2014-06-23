8f30a0ee767abef5a366f21713367d1459c87799
Bas de Ruiter <winkbrace@gmail.com>
2014-06-23 09:17:37 +0200

    set up problems 41 - 44

---
+++ problems/04/p041.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Pandigital prime
+ * Problem 41
+ *
+ * We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+ *
+ * What is the largest n-digit pandigital prime that exists?
+ *
+ * Solution: 7652413
+ */
+
+$log = new \Util\Log();
+
+$prime = \Math\Prime::getInstance();
+$com = new \Math\Combination();
+
+// get all combinations of 1 through n and check if they are prime
+for ($i=9; $i>0; $i--)
+{
+    $pool = '123456789';
+    $perms = $com->createPermutations(substr($pool, 0, $i));
+    $log->log('there are '.count($perms).' permutations of '.$i.' digits');
+
+    $panPrimes = array();
+    foreach ($perms as $n)
+    {
+        if ($prime->isPrime($n))
+            $panPrimes[] = $n;
+    }
+
+    $log->log('there are '.count($panPrimes).' pandigital primes of '.$i.' digits');
+    if (count($panPrimes) > 0)
+        break;
+}
+$log->solution(max($panPrimes));
